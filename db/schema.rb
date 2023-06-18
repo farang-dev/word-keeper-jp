bc8b932e137a89888ff9c62e623ad5e01d7385a6
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-06-18 19:06:07 +0900

    dev env works

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_104944) do
-
+ActiveRecord::Schema[7.0].define(version: 2023_06_18_100220) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
